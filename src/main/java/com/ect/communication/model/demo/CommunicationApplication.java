02fdfc563240a6027ff7e6a210a43782ffc9a6a0
Mehta, Dheeraj <mehta_dheeraj@optum.com>
2020-10-12 20:56:52 +0530

    Communication-Model:- config changes

---
+++ src/main/java/com/ect/communication/model/demo/CommunicationApplication.java
@@ -0,0 +1,14 @@
+package com.ect.communication.model.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CommunicationApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(CommunicationApplication.class, args);
+    }
+
+}
